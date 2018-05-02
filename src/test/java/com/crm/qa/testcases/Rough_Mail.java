7a5f4d29db0c03bb87133345413e768eb8083173
rampal singh <rampal.a.singh@gmail.com>
2018-05-02 16:57:22 +0530

    2-5-18

--- src/test/java/com/crm/qa/testcases/Rough_Mail.java
+++ src/test/java/com/crm/qa/testcases/Rough_Mail.java
@@ -1,6 +1,7 @@
 package com.crm.qa.testcases;
 
 import java.util.Properties;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
@@ -10,21 +11,21 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class Rough_Mail {
-	
-	public void sendMail() {
+
+	public static void main(String[] args) {
 
 		String host = "smtp.gmail.com";
 		final String user = "qaaurora1@gmail.com";// change accordingly
 		final String password = "energy123";// change accordingly
 
 		String to = "rampal.singh@sts.in";// change accordingly
-		String cc = "vishal.patel@sts.in";// change accordingly
+		// String cc = "vishal.patel@sts.in";// change accordingly
 
 		// Get the session object
 		Properties props = new Properties();
 		props.put("mail.smtp.host", host);
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "587"); //TLS Port
+		props.put("mail.smtp.port", "587"); // TLS Port
 		props.put("mail.smtp.starttls.enable", "true");
 
 		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
@@ -38,7 +39,9 @@ public class Rough_Mail {
 			MimeMessage message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(user));
 			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-			message.addRecipient(Message.RecipientType.CC, new InternetAddress(cc));
+			// message.addRecipient(Message.RecipientType.CC, new
+			// InternetAddress(cc));
+
 			message.setSubject("Test Mail");
 			message.setText("This is simple program of sending email using JavaMail API");
 
